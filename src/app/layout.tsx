89c2e3881119620ce07720fc022de080bf5c9b55
AntonValadzko1 <Anton.Valadzko@avisbudget.com>
2026-06-11 22:15:42 +0300

    Fix broken SSR

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,9 +2,6 @@ import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
 import './globals.css';
 import { Header } from '@/components/layout/Header';
-import { getServerEnv } from '@/lib/env';
-
-getServerEnv();
 
 const inter = Inter({ subsets: ['latin'], variable: '--font-geist-sans' });
 
