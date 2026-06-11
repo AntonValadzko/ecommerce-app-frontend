82760d62b1126c558b07e46eb4c77e082d72a3f8
AntonValadzko1 <Anton.Valadzko@avisbudget.com>
2026-06-11 23:09:52 +0300

    Clean-up

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -2,12 +2,9 @@ import type { StorybookConfig } from '@storybook/nextjs-vite';
 
 const config: StorybookConfig = {
   stories: ['../src/**/*.stories.@(js|jsx|mjs|ts|tsx)'],
-  "addons": [
-    "@chromatic-com/storybook",
-    "@storybook/addon-vitest",
-    "@storybook/addon-a11y",
-    "@storybook/addon-docs",
-    "@storybook/addon-mcp"
+  addons: [
+    '@storybook/addon-a11y',
+    '@storybook/addon-docs',
   ],
   "framework": "@storybook/nextjs-vite"
 };
